38d9a954da8188839a647cb19046f435d681de7a
TheSab1a <khurtsidze.saba@gmail.com>
2025-02-24 15:40:30 +0400

    first commit

---
+++ day 18/classwork/classwork.py
@@ -0,0 +1,13 @@
+for i in range(20, 41):
+    print(i)
+
+
+name = input("saba")  # მომხმარებელს შეჰყავს სახელი
+
+for i in range(15):  # ციკლი 15-ჯერ განმეორდება
+    print(name)  # ბეჭდავს შეყვანილ სახელს
+
+
+
+for num in range(2, 21, 2):
+    print(num)
