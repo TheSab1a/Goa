0ee036077b475d1f36a6dba0bcf611ad6cf6b744
TheSab1a <khurtsidze.saba@gmail.com>
2025-04-06 21:05:28 +0400

    first commit

---
+++ day 26/classwork/classwork.py
@@ -0,0 +1,21 @@
+user_name = input("saba")
+
+my_name = "saba"
+
+if user_name == my_name:
+    print("Hello")
+else:
+    print("Bye")
+
+
+
+
+for i in range(50, -1, -2):
+    print(i)
+
+
+
+surname = input("khurtsidze")
+
+for letter in surname:
+    print(letter)
